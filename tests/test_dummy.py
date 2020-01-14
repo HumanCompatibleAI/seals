44d3eb51be58003231365c685caca25d9b36926e
Adam Gleave <adam@gleave.me>
2020-01-13 21:55:59 -0800

    Lint docstrings

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,7 +1,10 @@
+"""Dummy file to make CI not complain. Delete once we have real tests!"""
+
 import pytest
 
 
 @pytest.mark.parametrize("i", range(20))
 def test_dummy(i):
+    """Dummy test to make CI not complain."""
     # TODO(adam): remove me once we have real tests!
     assert True
