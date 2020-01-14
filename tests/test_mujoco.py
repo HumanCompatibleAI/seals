f9cfa23889cd22e4ad2936fa90e5152872fd59da
Pedro Freire <pedrofreirex@gmail.com>
2020-01-14 23:35:23 +0100

    Use evaluate_policy from stable_baselines

--- tests/test_mujoco.py
+++ tests/test_mujoco.py
@@ -1,10 +1,12 @@
 """Test MuJoCo adaptations."""
 
+from stable_baselines.common.evaluation import evaluate_policy
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 
 import gym
+from gym.wrappers.time_limit import TimeLimit
 import benchmark_environments
 
 def test_fixed_env_model_as_good_as_gym_env_model():
@@ -21,31 +23,10 @@ def test_fixed_env_model_as_good_as_gym_env_model():
     fixed_model = PPO2(MlpPolicy, fixed_env, verbose=verbose)
     fixed_model.learn(total_timesteps=train_timesteps)
 
-    max_timesteps_per_episode = fixed_env.spec.max_episode_steps
-
-    gym_reward = _eval_model(gym_model, gym_env, num_episodes=num_eval_episodes,
-            max_timesteps_per_episode=max_timesteps_per_episode)
-    fixed_reward = _eval_model(fixed_model, fixed_env, num_episodes=num_eval_episodes,
-            max_timesteps_per_episode=max_timesteps_per_episode)
+    gym_env_eval = TimeLimit(gym_env, fixed_env.spec.max_episode_steps)
+    gym_reward, _ = evaluate_policy(gym_model, gym_env_eval)
+    fixed_reward, _ = evaluate_policy(fixed_model, fixed_env)
 
     epsilon = 0.1
     sign = 1 if gym_reward > 0 else -1
     assert (1 - sign * epsilon) * gym_reward <= fixed_reward
-
-def _eval_model(model, env, *, num_episodes, max_timesteps_per_episode):
-    total_re = 0
-
-    for episode in range(num_episodes):
-        ob = env.reset()
-        step = 0
-        done = False
-        while not done and step < max_timesteps_per_episode:
-            action, _ = model.predict(ob)
-            new_ob, re, done, _ = env.step(action)
-
-            total_re += re
-
-            ob = new_ob
-            step += 1
-
-    return total_re / num_episodes
