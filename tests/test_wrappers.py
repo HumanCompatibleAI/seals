b9d65f261478513a9fc8f829c52558238cbc7be1
Steven H. Wang <wang.steven.h@gmail.com>
2020-02-10 21:05:48 -0800

    test_wrappers: Test AutoResetWrapper manual reset

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -2,24 +2,25 @@ from benchmark_environments.testing.envs import CountingEnv
 from benchmark_environments.util import AutoResetWrapper
 
 
-def test_auto_reset_wrapper(episode_length=3, n_steps=100):
+def test_auto_reset_wrapper(episode_length=3, n_steps=100, n_manual_reset=2):
     env = AutoResetWrapper(CountingEnv(episode_length=episode_length))
 
-    obs = env.reset()
-    assert obs == 0
+    for _ in range(n_manual_reset):
+        obs = env.reset()
+        assert obs == 0
 
-    for t in range(1, n_steps + 1):
-        act = env.action_space.sample()
-        obs, rew, done, info = env.step(act)
-        expected_obs = t % episode_length
+        for t in range(1, n_steps + 1):
+            act = env.action_space.sample()
+            obs, rew, done, info = env.step(act)
+            expected_obs = t % episode_length
 
-        assert obs == expected_obs
-        assert done is False
+            assert obs == expected_obs
+            assert done is False
 
-        # End of episode
-        if expected_obs == 0:
-            assert info.get("terminal_observation", None) == episode_length
-            assert rew == episode_length * 10
-        else:
-            assert "terminal_obs" not in info
-            assert rew == expected_obs * 10
+            # End of episode
+            if expected_obs == 0:
+                assert info.get("terminal_observation", None) == episode_length
+                assert rew == episode_length * 10
+            else:
+                assert "terminal_obs" not in info
+                assert rew == expected_obs * 10
