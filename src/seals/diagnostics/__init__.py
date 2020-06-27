9273fae238fcc0a27b3e117a551526a708ce8b55
Pedro Freire <pedrofreirex@gmail.com>
2020-06-27 10:30:09 -0700

    Add Sort environment.

--- src/seals/diagnostics/__init__.py
+++ src/seals/diagnostics/__init__.py
@@ -7,3 +7,9 @@ gym.register(
     entry_point="seals.diagnostics.risky_path:RiskyPathEnv",
     max_episode_steps=5,
 )
+
+gym.register(
+    id="seals/Sort-v0",
+    entry_point="seals.diagnostics.sort:SortEnv",
+    max_episode_steps=6,
+)
