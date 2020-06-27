ab07bebfbac938e6136e2e18622645910f81aac4
Pedro Freire <pedrofreirex@gmail.com>
2020-06-27 09:21:50 -0700

    Add LargestSumEnv.

--- src/seals/diagnostics/__init__.py
+++ src/seals/diagnostics/__init__.py
@@ -7,3 +7,9 @@ gym.register(
     entry_point="seals.diagnostics.risky_path:RiskyPathEnv",
     max_episode_steps=5,
 )
+
+gym.register(
+    id="seals/LargestSum-v0",
+    entry_point="seals.diagnostics.largest_sum:LargestSumEnv",
+    max_episode_steps=1,
+)
