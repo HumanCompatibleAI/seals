bd6680a849ae01f5d56e354b4c977cf9648ff930
Pedro Freire <pedrofreirex@gmail.com>
2020-06-27 09:30:19 -0700

    Add EarlyTerm envs.

--- src/seals/diagnostics/__init__.py
+++ src/seals/diagnostics/__init__.py
@@ -7,3 +7,15 @@ gym.register(
     entry_point="seals.diagnostics.risky_path:RiskyPathEnv",
     max_episode_steps=5,
 )
+
+gym.register(
+    id="seals/EarlyTermPos-v0",
+    entry_point="seals.diagnostics.early_term:EarlyTermPosEnv",
+    max_episode_steps=10,
+)
+
+gym.register(
+    id="seals/EarlyTermNeg-v0",
+    entry_point="seals.diagnostics.early_term:EarlyTermNegEnv",
+    max_episode_steps=10,
+)
---
+++ src/seals/diagnostics/early_term.py
@@ -0,0 +1,58 @@
+"""Environment checking for correctness under early termination."""
+
+from functools import partial
+
+import numpy as np
+
+from seals import base_envs
+
+
+class EarlyTerminationEnv(base_envs.TabularModelPOMDP):
+    """Three-state MDP with early termination state.
+
+    Many implementations of imitation learning algorithms incorrectly assign a
+    value of zero to terminal states (Kostrikov et. al (2018)).  Depending on
+    the sign of the learned reward function in non-terminal states, this can
+    either bias the agent to end episodes early or prolong them as long as
+    possible.  This confounds evaluation as performance is spuriously high in
+    tasks where the termination bias aligns with the task objective.  These
+    tasks attempt to detect this type of bias, and they are adapted from
+    Kostrikov et. al (2018).
+
+    The environment is a 3-state MDP, in which the agent can either alternate
+    between two initial states until reaching the time horizon, or they can
+    move to a terminal state causing the episode to terminate early.
+    """
+
+    def __init__(self, is_reward_positive: bool = True):
+        """Construct environment.
+
+        Args:
+            is_reward_positive: whether rewards are positive or negative.
+        """
+        nS = 3
+        nA = 2
+
+        transition_matrix = np.zeros((nS, nA, nS))
+
+        transition_matrix[0, :, 1] = 1.0
+
+        transition_matrix[1, 0, 0] = 1.0
+        transition_matrix[1, 1, 2] = 1.0
+
+        transition_matrix[2, :, 2] = 1.0
+
+        reward_sign = 2 * is_reward_positive - 1
+        reward_matrix = reward_sign * np.ones((nS,))
+
+        super().__init__(
+            transition_matrix=transition_matrix, reward_matrix=reward_matrix,
+        )
+
+    def terminal(self, state: int, n_actions_taken: int) -> bool:
+        """Returns True if (and only if) in state 2."""
+        return state == 2
+
+
+EarlyTermPosEnv = partial(EarlyTerminationEnv, is_reward_positive=True)
+EarlyTermNegEnv = partial(EarlyTerminationEnv, is_reward_positive=False)
