749d41f2d928c0ab42546a8062b172da19bd7187
Pedro Freire <pedrofreirex@gmail.com>
2020-06-27 10:48:18 -0700

    Add ProcGoal environment.

--- src/seals/diagnostics/__init__.py
+++ src/seals/diagnostics/__init__.py
@@ -7,3 +7,9 @@ gym.register(
     entry_point="seals.diagnostics.risky_path:RiskyPathEnv",
     max_episode_steps=5,
 )
+
+gym.register(
+    id="seals/ProcGoal-v0",
+    entry_point="seals.diagnostics.proc_goal:ProcGoalEnv",
+    max_episode_steps=20,
+)
