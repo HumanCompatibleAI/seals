625c1e10f58c655d4b0c7add7ab676e063ccda0c
pedrofreire <pedrofreirex@gmail.com>
2020-06-30 23:32:51 +0200

    Add InitShift. (#24)
    * Add InitShift envs.
    
    * Add InitShift to deterministic envs
    
    * Apply suggestions from code review
    
    Co-authored-by: Adam Gleave <adam@gleave.me>
    
    * Rename InitShift envs, add param validation
    
    * Update env names in DETERMINISTIC_ENVS
    
    * Improve test coverage
    
    * Fix missing parenthesis
    
    Co-authored-by: Adam Gleave <adam@gleave.me>

--- src/seals/diagnostics/__init__.py
+++ src/seals/diagnostics/__init__.py
@@ -9,6 +9,18 @@ gym.register(
 )
 
 gym.register(
+    id="seals/InitShiftTrain-v0",
+    entry_point="seals.diagnostics.init_shift:InitShiftTrainEnv",
+    max_episode_steps=3,
+)
+
+gym.register(
+    id="seals/InitShiftTest-v0",
+    entry_point="seals.diagnostics.init_shift:InitShiftTestEnv",
+    max_episode_steps=3,
+)
+
+gym.register(
     id="seals/NoisyObs-v0",
     entry_point="seals.diagnostics.noisy_obs:NoisyObsEnv",
     max_episode_steps=15,
