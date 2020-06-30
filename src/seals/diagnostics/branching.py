741b773c05f54c5eca35d6b8b2c8a35a15a98f8d
Pedro Freire <pedrofreirex@gmail.com>
2020-06-30 06:56:15 -0700

    Move get_next to __init__

--- src/seals/diagnostics/branching.py
+++ src/seals/diagnostics/branching.py
@@ -22,15 +22,16 @@ class BranchingEnv(base_envs.TabularModelPOMDP):
             branch_factor: number of actions at each state.
             length: path length from initial state to goal.
         """
-        self._branch_factor = branch_factor
-
         nS = 1 + branch_factor * length
-        self._n_states = nS
         nA = branch_factor
 
+        def get_next(state: int, action: int) -> int:
+            can_move = state % branch_factor == 0 and state != nS - 1
+            return state + (action + 1) * can_move
+
         transition_matrix = np.zeros((nS, nA, nS))
-        for ob, act in itertools.product(range(nS), range(nA)):
-            transition_matrix[ob, act, self._get_next(ob, act)] = 1.0
+        for state, action in itertools.product(range(nS), range(nA)):
+            transition_matrix[state, action, get_next(state, action)] = 1.0
 
         reward_matrix = np.zeros((nS,))
         reward_matrix[-1] = 1.0
@@ -38,8 +39,3 @@ class BranchingEnv(base_envs.TabularModelPOMDP):
         super().__init__(
             transition_matrix=transition_matrix, reward_matrix=reward_matrix,
         )
-
-    def _get_next(self, state: int, action: int) -> int:
-        b = self._branch_factor
-        n = self._n_states
-        return state + (action + 1) * int(state % b == 0 and state != n - 1)
