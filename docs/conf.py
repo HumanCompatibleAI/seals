08b717b5d767b66019111acc5967aef1824d14b7
Adam Gleave <adam@gleave.me>
2020-07-07 15:48:21 -0700

    Add logo (#33)
    * Add logo
    
    * Update README.md
    
    png->svg
    
    * Update README.md
    
    Remove align which is having no effect
    
    * Change background color in docs

--- docs/conf.py
+++ docs/conf.py
@@ -61,8 +61,13 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+html_logo = "_static/img/logo.svg"
+html_theme_options = {
+    "logo_only": True,
+    "style_nav_header_background": "#06203A",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["_static"]
