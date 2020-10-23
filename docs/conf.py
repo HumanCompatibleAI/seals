498195e3fee119c2435dd54fa9bad1c8828d472a
Adam Gleave <adam@gleave.me>
2020-10-23 15:48:52 +0100

    Lint docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,15 @@
-# Configuration file for the Sphinx documentation builder.
-#
+"""Configuration file for the Sphinx documentation builder."""
+
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import seals
 
 # -- Project information -----------------------------------------------------
 
-import seals
-
 project = "seals"
-copyright = "2020, Center for Human-Compatible AI"
+copyright = "2020, Center for Human-Compatible AI"  # noqa: A001
 author = "Center for Human-Compatible AI"
 
 # The full version, including alpha/beta/rc tags
