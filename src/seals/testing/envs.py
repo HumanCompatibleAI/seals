b8f16023a5ba4d03cf643fbe86da3eed12413e9d
Adam Gleave <adam@gleave.me>
2021-08-20 15:40:29 -0700

    Remove stray no cover

--- src/seals/testing/envs.py
+++ src/seals/testing/envs.py
@@ -277,7 +277,7 @@ def test_render(env: gym.Env, raises_fn) -> None:
         # on the viewer (commented out) so the resources are not released.
         # For now this is OK, but may bite if we end up testing a lot of
         # MuJoCo environments.
-        is_mujoco = _is_mujoco_env(env)  # pragma: no cover
+        is_mujoco = _is_mujoco_env(env)
         if "rgb_array" in render_modes and not is_mujoco:
             # Render should not change without calling `step()`.
             # MuJoCo rendering fails this check, ignore -- not much we can do.
