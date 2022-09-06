0e50cd04d6a1a20136eec5f54d227cde346a42bf
Daniel Filan <df@danielfilan.com>
2022-09-06 14:16:41 -0700

    Add type annotations

--- src/seals/atari.py
+++ src/seals/atari.py
@@ -1,16 +1,18 @@
 """Adaptation of Atari environments for specification learning algorithms."""
 
+from typing import Iterable
+
 import gym
 
 from seals.util import AutoResetWrapper, get_gym_max_episode_steps
 
 
-def fixed_length_atari(atari_env_id):
+def fixed_length_atari(atari_env_id: str) -> gym.Env:
     """Fixed-length variant of a given Atari environment."""
     return AutoResetWrapper(gym.make(atari_env_id))
 
 
-def _not_ram_or_det(env_id):
+def _not_ram_or_det(env_id: str) -> bool:
     """Checks a gym Atari environment isn't deterministic or using RAM observations."""
     slash_separated = env_id.split("/")
     # environment name should look like "ALE/Amidar-v5" or "Amidar-ramNoFrameskip-v4"
@@ -23,7 +25,7 @@ def _not_ram_or_det(env_id):
     return not_ram and not_deterministic
 
 
-def _supported_atari_env(gym_spec):
+def _supported_atari_env(gym_spec: gym.envs.registration.EnvSpec) -> bool:
     """Checks if a gym Atari environment is one of the ones we will support."""
     is_atari = gym_spec.entry_point == "gym.envs.atari:AtariEnv"
     v5_and_plain = gym_spec.id.endswith("-v5") and not ("NoFrameskip" in gym_spec.id)
@@ -35,13 +37,15 @@ def _supported_atari_env(gym_spec):
     )
 
 
-def _seals_name(gym_spec):
+def _seals_name(gym_spec: gym.envs.registration.EnvSpec) -> str:
     """Makes a Gym ID for an Atari environment in the seals namespace."""
     slash_separated = gym_spec.id.split("/")
     return "seals/" + slash_separated[-1]
 
 
-def register_atari_envs(gym_atari_env_specs):
+def register_atari_envs(
+    gym_atari_env_specs: Iterable[gym.envs.registration.EnvSpec],
+) -> None:
     """Register wrapped gym Atari environments."""
     for gym_spec in gym_atari_env_specs:
         gym.register(
